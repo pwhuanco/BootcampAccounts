38c918fbfd5bbae45dbf1df1dcb28d5a452c7ce6
jlamaspi <juliolamas4795@gmail.com>
2022-03-21 11:43:46 -0500

    Agregado monto mínimo de apertura de cuenta

--- bankaccount/src/main/java/com/bootcamp/bankaccount/models/bean/Account.java
+++ bankaccount/src/main/java/com/bootcamp/bankaccount/models/bean/Account.java
@@ -43,4 +43,8 @@ public class Account {
 
     private String clientIdNumber;
 
+    private Double minimumOpeningAmount;
+
+
+
 }
--- bankaccount/src/main/java/com/bootcamp/bankaccount/models/dto/AccountDto.java
+++ bankaccount/src/main/java/com/bootcamp/bankaccount/models/dto/AccountDto.java
@@ -32,5 +32,7 @@ public class AccountDto {
 
     private String clientIdNumber;
 
+    private Double minimumOpeningAmount;
+
 
 }
